1a00075c961cec51f11e77ef6a690675d6bf84dd
jeseong <jeseonglee77@gmail.com>
2025-05-25 14:42:58 +0900

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="flex flex-row p-4 justify-between items-center border-b border-gray-300">
+      <h1 className="text-2xl font-bold">Bubble</h1>
+      <button className="px-4 py-2 bg-black text-white rounded hover:bg-neutral-500">
+        download
+      </button>
+    </div>
+  );
+}
